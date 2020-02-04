5874c291189cd5bc172317d52b07528a7a371c9c
Walter Eoin Holohan <walter.holohan@gmail.com>
2020-02-04 21:54:21 +0000

    eject app

--- App.tsx
+++ App.tsx
@@ -1,9 +1,6 @@
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
-import React, { useState } from 'react';
+// import { Asset } from 'expo-asset';
+import React, { useState, useEffect } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
 
 import AppNavigator from './src/navigation/AppNavigator';
 
@@ -14,40 +11,22 @@ const styles = StyleSheet.create({
   },
 });
 
-async function loadResourcesAsync() {
-  await Promise.all([
-    Asset.loadAsync([require('./assets/images/robot-dev.png'), require('./assets/images/robot-prod.png')]),
-    Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-    }),
-  ]);
-}
+// async function loadResourcesAsync() {
+//   await Promise.all([
+//     Asset.loadAsync([require('./assets/images/robot-dev.png'), require('./assets/images/robot-prod.png')]),
+//   ]);
+// }
 
-function handleLoadingError(error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
-  console.warn(error);
-}
-
-function handleFinishLoading(setLoadingComplete) {
-  setLoadingComplete(true);
-}
-
-export default function App(props) {
+export default function App() {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
 
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return (
-      <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
-      />
-    );
+  useEffect(() => {
+    // SplashScreen.hide();
+    setLoadingComplete(true);
+  }, []);
+
+  if (!isLoadingComplete) {
+    return null;
   } else {
     return (
       <View style={styles.container}>
--- src/components/StyledText.tsx
+++ src/components/StyledText.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { Text } from 'react-native';
 
 export function MonoText(props): JSX.Element {
-  return <Text {...props} style={[props.style, { fontFamily: 'space-mono' }]} />;
+  return <Text {...props} style={[props.style]} />;
 }
