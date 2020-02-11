bcc2bdda0cc52dce07a7e850b94f4cadf87ce3b1
Walter Eoin Holohan <walter.holohan@gmail.com>
2020-02-11 22:53:34 +0000

    change refs from loki to my-app

--- android/app/src/main/java/com/loki/MainActivity.java
+++ android/app/src/main/java/com/loki/MainActivity.java
@@ -1,4 +1,4 @@
-package com.loki;
+package com.my-app;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "loki";
+        return "my-app";
     }
 
     @Override
