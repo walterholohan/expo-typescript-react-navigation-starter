cab8330299091dda99cb283dd8d5a961047c47ac
Walter Eoin Holohan <walter.holohan@gmail.com>
2020-02-16 10:07:12 +0000

    add rn native base

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import 'react-native-gesture-handler';
 import { Asset } from 'expo-asset';
 import React, { useState, useEffect } from 'react';
+import * as Font from 'expo-font';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 import AppNavigator from './src/navigation/AppNavigator';
 
@@ -15,6 +17,12 @@ const styles = StyleSheet.create({
 async function loadResourcesAsync() {
   await Promise.all([
     Asset.loadAsync([require('./assets/images/robot-dev.png'), require('./assets/images/robot-prod.png')]),
+    Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    }),
   ]);
 }
 
